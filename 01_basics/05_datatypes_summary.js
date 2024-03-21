f1d8644e8877f772a250d141ba5f5de257e7f5bb
Abhishek121 <159711947+AK0121@users.noreply.github.com>
2024-03-21 13:52:56 +0000

    datatype summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,51 @@
+ /*
+There are two major categories of dataTypes in javaScript :
+
+
+Primitive :-
+number = 2,3,34,23.5,123,3435.
+string = "string_value"
+bigint 
+boolean = true/false.
+null = Stand alone value.
+undefined = 
+symbol = unique.
+
+
+// javascript is a dynamically typed language (that means datatype is assigned automatically at the time of code execution and we can change it later throughout whole program) :-
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const temp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 34463543458748429n
+
+*/
+
+// Non-Primitive or Reference :-
+// Objects
+// Arrays
+// Functions
+
+
+// const heros = ["shaktiman","naagraaj","doga"];
+
+// let myObj = {
+//     name:"Abhi",
+//     age: 22,
+// }
+
+// const myFunction = function() {
+//     console.log("Hello world");
+// }
+const temp = null
+console.log(typeof temp);
+
