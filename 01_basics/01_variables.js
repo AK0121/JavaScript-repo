22b210c0ade9e583163403b1e5cf8a601e755ea8
Abhishek121 <159711947+AK0121@users.noreply.github.com>
2024-03-21 06:21:33 +0000

    Variables and DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553;
+let accountEmail = "Abhishek@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+let accountState;
+
+accountEmail = "hach@123amil.com"
+accountPassword = "28371"
+accountCity = "Delhi"
+// accountId = 2;
+
+console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); //We are using nodejs, not browser
+
+console.log("Hello world");
+console.log(3 + 3);
+
+/*
+There are two major categories of dataTypes in javaScript :
+Primitive :-
+number = 2,3,34,23.5,123,3435.
+string = "string_value"
+bigint 
+boolean = true/false.
+null = Stand alone value.
+undefined = 
+symbol = unique.
+
+Non-Primitive :-
+Object
+Arrays
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
