81a0e194acf07c5be9a06b6a4591d75bea005a8c
Abhishek121 <159711947+AK0121@users.noreply.github.com>
2024-03-23 10:51:22 +0000

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.4535
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// *********************************** Maths ****************************
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //IT CONVERT AND RETURNS THE VALUE IN POSITIVE 
+
+// console.log(Math.round(4.5)); // RETURNS THE ROUND OFF VALUE
+// console.log(Math.ceil(4.2));  // RETURNS THE ROUND OFF VALUE (ONLY SUCCECIDING  VALUE)
+// console.log(Math.floor(4.9)); // RETURNS THE ROUND OFF VALUE (ONLY ORIGINAL VALUE)
+
+// console.log(Math.min(4, 5, 2, 7, 1)); // RETURNS THE SMALLEST NUMBER FROM AN ARRAY
+// console.log(Math.max(4, 5, 2, 7, 1)); // RETURNS THE LARGEST NUMBER FROM AN ARRAY
+
+
+// Important!!!!!!!!!!
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
