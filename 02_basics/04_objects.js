d109d085a33ead0093c5dc7531c192dc179ae9f5
Abhishek121 <159711947+AK0121@users.noreply.github.com>
2024-03-26 16:36:47 +0000

    jason brief intro

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,87 @@
+// // const tinderUser = new Object()   // This is a singleton object
+// const tinderUser = {}
+
+// tinderUser.id = "123abc"
+// tinderUser.name = "Abhi"
+// tinderUser.isLoggedIN = "true"
+
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname: {
+//         userFullName: {
+//             firstName: "Abhishek",
+//             lastName: "121"
+//         }
+//     }
+// }
+
+// // console.log(regularUser.fullname.userFullName.firstName);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+
+// // const obj3 = {obj1, obj2}
+// // const obj3 = Object.assign ({}, obj1, obj2, obj4)
+
+// const obj3 = {...obj1,...obj2,...obj4};
+// // console.log(obj3)
+
+
+// const users = [
+//     {
+//         id: 1,
+//         email : "X@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email : "X@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email : "X@gmail.com"
+//     },
+// ]
+
+// console.log(users[1].email);
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+// console.log(tinderUser.hasOwnProperty("lala"));
+
+
+
+// Destructuring in JavaScript:-
+
+// const course = {
+//     name:"js in hindi",
+//     price: "999",
+//     courseInstructor: "AK"
+// }
+
+// // course.courseInstructor
+
+// const {courseInstructor: instructor} = course;
+
+// // console.log(courseInstructor);
+// console.log(instructor);
+
+
+// API's in javascript jason
+
+// {
+//     "name": "Abhishek",
+//     "courseName": "js in hindi",
+//     "price": "free",
+// }
+
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
