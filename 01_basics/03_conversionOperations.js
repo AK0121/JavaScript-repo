615fbfc8d0569a30ecc4108ec8d50246158aa795
Abhishek121 <159711947+AK0121@users.noreply.github.com>
2024-04-04 10:58:19 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -12,8 +12,39 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
-let someNumber = true;
+// let someNumber = true;
 
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************** Operations ****************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " abhi"
+
+// let str3 = str1 + str2
+// // console.log(str3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2;
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
