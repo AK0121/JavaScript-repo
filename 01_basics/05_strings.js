e798bf7380fc49cf552c1eec56f1b78d6e623b52
Abhishek121 <159711947+AK0121@users.noreply.github.com>
2024-04-04 10:58:46 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+// const name = "Jolly"
+// const repoCount = 50
+
+// console.log(name + repoCount + "value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("Indian-pro");
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("t"));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-10, 5)
+// console.log(anotherString);
+
+// const newStringOne = "  hitesh  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://ak.com/abhishek%20saini"
+
+// console.log(url.replace("%20", "-"));
+
+// console.log(url.includes("sharma"));
+
+// console.log(gameName.split("-"));
+
+let randomstr = "";
+console.log(randomstr.length);
